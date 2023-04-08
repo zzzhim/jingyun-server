90881dd772b1d0dfa9b08d412c7c7010a9468867
zzzhim <3218566389@qq.com>
2023-04-08 14:47:40 +0800

    build：初始化

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1680935194403_8969',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
